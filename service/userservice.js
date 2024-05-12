720d8575254b79164a47941fa901ad4195bab83b
Hilkiah-S <kelkiasselamu@yahoo.com>
2024-05-12 08:24:18 +0300

    unquiness droped from profile

--- service/userservice.js
+++ service/userservice.js
@@ -1,8 +1,9 @@
 const userModel = require("../models/usermodel.ts")
 
-
+require('dotenv').config();
 async function createaccount(username, profilepic) {
-profilepic='https://numberquest.addisphoenix.com/uploads/'+profilepic+'.png';
+    console.log("sec storage",process.env.SEC_STORE);
+profilepic=process.env.SEC_STORE+profilepic+'.png';
 
     const user = await userModel.create({ username,profilepicture:profilepic})
     if(!user){
@@ -13,7 +14,24 @@ profilepic='https://numberquest.addisphoenix.com/uploads/'+profilepic+'.png';
 
 async function checkUsernameAvaliablity(username) {
 
-
+    // await userModel.collection.getIndexes().then(indexes => {
+    //     console.log(indexes);
+    // }).catch(err => {
+    //     console.error('Error fetching indexes:', err);
+    // });
+    // await userModel.collection.dropIndex('profilepicture_1').then(() => {
+    //     console.log('Unique index on profilepicture dropped.');
+    // }).catch(err => {
+    //     console.error('Error dropping index:', err);
+    // });
+    
+    // await userModel.collection.createIndex({ profilepicture: 1 }).then(() => {
+    //     console.log('Index on profilepicture recreated without uniqueness.');
+    // }).catch(err => {
+    //     console.error('Error creating index:', err);
+    // });
+    
+    
     const user = await userModel.findOne({ username})
     if(!user){
         return true;
