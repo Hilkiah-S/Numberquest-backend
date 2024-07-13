8bcc2c51e70e37659b602aef4a6b4f557202698d
Hilkiah_S <kelkiasselamu@yahoo.com>
2024-07-14 00:43:42 +0300

    adjusting the ports

--- index.js
+++ index.js
@@ -1,76 +1,142 @@
-const express= require('express')
-const app = express()
-const userRouter = require('./routes/user')
-const ScoreRouter = require('./routes/score')
-const mongoose = require('mongoose')
-const bodyparser = require('body-parser');
-const cors =require("cors")
-require('dotenv').config();
-const axios = require('axios');
-const socketIo = require('socket.io');
-const http = require('http');
-const server = http.createServer(app);
-const io = socketIo(server);
-const { v4: uuidv4 } = require('uuid');  
-mongoose.connect(process.env.MONGO_URI).then(
-    ()=>{
-        console.log("Connectd to MongoDB")
-        app.listen(process.env.PORT,()=>{
-            console.log('Node running on port'+process.env.PORT);
+// const express= require('express')
+// const app = express()
+// const userRouter = require('./routes/user')
+// const ScoreRouter = require('./routes/score')
+// const mongoose = require('mongoose')
+// const bodyparser = require('body-parser');
+// const cors =require("cors")
+// require('dotenv').config();
+// const axios = require('axios');
+// const socketIo = require('socket.io');
+// const http = require('http');
+// const server = http.createServer(app);
+// const io = socketIo(server);
+// const { v4: uuidv4 } = require('uuid');  
+// mongoose.connect(process.env.MONGO_URI).then(
+//     ()=>{
+//         console.log("Connectd to MongoDB")
+//         app.listen(process.env.PORT,()=>{
+//             console.log('Node running on port'+process.env.PORT);
             
-        } );
-        // process.env.IOPORT
-    server.listen(3001, () => {
-        console.log('Node and Socket.IO running on port ' + 3001);
-    });
-    }
-).catch(
-    (error)=>{
-        console.log(error)
-    }
-)
+//         } );
+//         // process.env.IOPORT
+//     server.listen(3001, () => {
+//         console.log('Node and Socket.IO running on port ' + 3001);
+//     });
+//     }
+// ).catch(
+//     (error)=>{
+//         console.log(error)
+//     }
+// )
 
 
 
-app.use(cors());
-app.use(bodyparser.json())
-app.use((req,res,next)=>{
-    console.log(req.body);
-    next();
-})
-app.use('/user',userRouter)
+// app.use(cors());
+// app.use(bodyparser.json())
+// app.use((req,res,next)=>{
+//     console.log(req.body);
+//     next();
+// })
+// app.use('/user',userRouter)
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
     
   
 
-    socket.on('createRoom', () => {
-        const roomId = uuidv4(); 
-        socket.join(roomId);
-        socket.emit('roomID', roomId); 
-        console.log(`Room created with ID: ${roomId}`);
-    });
+//     socket.on('createRoom', () => {
+//         const roomId = uuidv4(); 
+//         socket.join(roomId);
+//         socket.emit('roomID', roomId); 
+//         console.log(`Room created with ID: ${roomId}`);
+//     });
 
-    socket.on('join', (roomId) => {
-        socket.join(roomId);
-        console.log(`User joined room ${roomId}`);
-    });
+//     socket.on('join', (roomId) => {
+//         socket.join(roomId);
+//         console.log(`User joined room ${roomId}`);
+//     });
 
-    socket.on('message', (data) => {
-        io.to(data.room).emit('message', data.msg);
-    });
+//     socket.on('message', (data) => {
+//         io.to(data.room).emit('message', data.msg);
+//     });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected');
+//     });
+// });
+// // app.use('/score',ScoreRouter)
+
+// app.use((err,req,res,next)=>{
+// if(err.name!='UnauthorizedError'){
+//     return next(err);
+// }
+// res.status(401).json({"success":false,"msg":"Unauthenticated"});
+// });
+
+const express = require('express');
+const app = express();
+const userRouter = require('./routes/user');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const http = require('http');
+const socketIo = require('socket.io');
+const { v4: uuidv4 } = require('uuid');
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
-// app.use('/score',ScoreRouter)
 
-app.use((err,req,res,next)=>{
-if(err.name!='UnauthorizedError'){
+mongoose.connect(process.env.MONGO_URI).then(
+  () => {
+    console.log("Connected to MongoDB");
+    server.listen(process.env.PORT || 3000, () => {
+      console.log(`Server running on port ${process.env.PORT || 3000}`);
+    });
+  }
+).catch(
+  (error) => {
+    console.log(error);
+  }
+);
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use('/user', userRouter);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  socket.on('createRoom', () => {
+    const roomId = uuidv4();
+    socket.join(roomId);
+    socket.emit('roomID', roomId);
+    console.log(`Room created with ID: ${roomId}`);
+  });
+
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+    console.log(`User joined room ${roomId}`);
+  });
+
+  socket.on('message', (data) => {
+    io.to(data.room).emit('message', data.msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+app.use((err, req, res, next) => {
+  if (err.name !== 'UnauthorizedError') {
     return next(err);
-}
-res.status(401).json({"success":false,"msg":"Unauthenticated"});
+  }
+  res.status(401).json({ "success": false, "msg": "Unauthenticated" });
 });
-
